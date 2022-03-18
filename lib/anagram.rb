7f3408f89aaa6e9b0cf986d420aaeaedefb7801b
+ <+>
2022-03-18 13:51:22 -0400

    commit tree 6cbf33c0a6e66c048a743830842a440178ee890a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(array)
+        array.filter do |inserted_word|
+             
+             inserted_word.chars.sort == @word.chars.sort
+        end 
+    end 
+end 
